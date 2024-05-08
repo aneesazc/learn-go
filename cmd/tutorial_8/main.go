67014a97f8f556a1f4d58df85d690d9b69dd0773
aneesazc <anees.azc@gmail.com>
2024-05-08 10:37:07 +0530

    channels

--- cmd/tutorial_8/main.go
+++ cmd/tutorial_8/main.go
@@ -1,20 +1,77 @@
 package main
 
-// channels are used to communicate between goroutines
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-import "fmt"
+var MAX_CHICKEN_PRICE float32 = 5
+var MAX_POTATO_PRICE float32 = 3
 
 func main() {
-	// create a channel
-	c := make(chan string)
-
-	// send a value to the channel
-	go process(c)
-
-	msg := <-c
-	fmt.Println(msg)
+	var chickenChannel = make(chan string)
+	var potatoChannel = make(chan string)
+	var websites = []string{"walmart.com", "costco.com", "wholefoods.com"}
+	for i := range websites {
+		go checkChickenPrices(websites[i], chickenChannel)
+		go checkPotatoPrices(websites[i], potatoChannel)
+	}
+	sendMessage(chickenChannel, potatoChannel)
 }
 
-func process(c chan string) {
-	c <- "Hello, World!"
+func checkPotatoPrices(website string, potatoChannel chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var potatoPrice = rand.Float32() * 10
+		if potatoPrice <= MAX_POTATO_PRICE {
+			potatoChannel <- website
+			break
+		}
+	}
+
 }
+
+func checkChickenPrices(website string, chickenChannel chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var chickenPrice = rand.Float32() * 20
+		if chickenPrice <= MAX_CHICKEN_PRICE {
+			chickenChannel <- website
+			break
+		}
+	}
+}
+
+func sendMessage(chickenChannel chan string, potatoChannel chan string) {
+	select {
+	case website := <-chickenChannel:
+		fmt.Printf("\nText Sent: Food deal on chicken at %v.", website)
+	case website := <-potatoChannel:
+		fmt.Printf("\nEmail Sent: Food deal on potatoes at %v.", website)
+	}
+}
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	c := make(chan int, 5)
+// 	go process(c)
+// 	for i := range c {
+// 		fmt.Println("Received:", i)
+// 		time.Sleep(time.Second * 1)
+// 	}
+// }
+
+// func process(c chan int) {
+// 	defer close(c)
+// 	for i := 0; i < 5; i++ {
+// 		c <- i
+// 	}
+// 	fmt.Println("Exiting process")
+// }
